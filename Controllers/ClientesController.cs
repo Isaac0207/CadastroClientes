8c2681d24bc2cd4c34cccf4c7c1350f6ab9f8c08
Isaac0207 <isaacxsilva7@gmail.com>
2024-11-19 21:37:03 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/ClientesController.cs
@@ -0,0 +1,93 @@
+﻿using CadastroClientes.Models;
+using CadastroClientes.Models.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroClientes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        [HttpPost("Salvar")]
+        public object Salvar([FromBody] Clientes cadastro)
+        {
+            try
+            {
+                ClientesRepository clientes = new ClientesRepository();
+                clientes.Salvar(cadastro);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return null;
+        }
+        [HttpPost("Alterar")]
+        public object Alterar([FromBody] Clientes clientes)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return null;
+        }
+
+        [HttpGet("Listar")]
+        public object Listar()
+        {
+            List<Clientes> listaCli = null;
+            try
+            {
+                ClientesRepository clientesRepo = new ClientesRepository();
+                listaCli = clientesRepo.Listar();
+            }
+            catch (Exception ex)
+            {
+
+                
+            }
+            return listaCli;
+        }
+        [HttpDelete ("Deletar")]
+            public object Deletar(string Documento)
+        {
+            try
+            {
+                ClientesRepository clientes = new ClientesRepository();
+                bool retornoDelete = clientes.Deletar(Documento);
+            }
+            catch(Exception ex) 
+            {
+
+            }
+            return null;
+        }
+
+        [HttpGet("GetCliente")]
+        public object GetCliente(string Documento)
+        {
+            try
+            {
+             
+               ClientesRepository cliente = new ClientesRepository();
+                var returno = cliente.GetCliente(Documento);
+                return returno;
+
+ 
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return null;
+        }
+
+
+    }
+
+}
---
+++ Models/Repository/ClientesRepository.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace CadastroClientes.Models.Repository
+{
+    public class ClientesRepository
+    {
+
+        public void Salvar(Clientes clientes)
+        {
+
+            var listaClientes = Listar();
+            var item = listaClientes.Where(t => t.Documento == clientes.Documento).FirstOrDefault();
+         
+            //se ele existe eu deleto
+            if (item != null)
+            {
+                Deletar(clientes.Documento);
+            }
+         var clientesTexto = JsonConvert.SerializeObject(clientes) + "," + Environment.NewLine;
+           
+            File.AppendAllText("C:\\Users\\drago\\source\\repos\\CadastroClientes\\BancoDados\\BancoDados.txt", clientesTexto );
+
+        }
+        public List<Clientes> Listar()
+        {
+
+         var clientes = File.ReadAllText("C:\\Users\\drago\\source\\repos\\CadastroClientes\\BancoDados\\BancoDados.txt");
+
+            List <Clientes> clientesLista = JsonConvert.DeserializeObject<List<Clientes>>("[" + clientes + "]");
+
+
+            return clientesLista.OrderByDescending(t=>t.Nome).ToList();
+        }
+        public bool Deletar (string Documento)
+        {
+            //listei todos os itens
+            var listaClientes = Listar();
+            var item = listaClientes.Where(t => t.Documento == Documento).FirstOrDefault();
+            if (item != null) 
+                //remover itens da lista
+            { 
+                listaClientes.Remove(item);
+                //limpar banco de dados
+                File.WriteAllText("C:\\Users\\drago\\source\\repos\\CadastroClientes\\BancoDados\\BancoDados.txt", string.Empty);
+                //escrever novamente o banco de dados a lista sem o item excluido
+                foreach(var  cliente in listaClientes)
+                {
+                    Salvar(cliente);
+                }
+                return true;
+
+            }
+            return false;
+
+        }
+        public Clientes GetCliente(string Documento)
+        {
+            var clienteLista = Listar();
+            var item = clienteLista.Where(t => t.Documento == Documento).FirstOrDefault();
+            return item;    
+        }
+        
+    }
+}
